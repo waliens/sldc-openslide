6be69b406665ad10806e926636486113c0b3103b
Romain Mormont <romainmormont@hotmail.com>
2017-03-07 09:14:17 +0100

    add travis + codecov + setup.py files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="sldc-openslide",
+    version="1.0",
+    description="SLDC-OpenSlide, a binding for using the OpenSlide image library with SLDC.",
+    author="Romain Mormont",
+    author_email="romain.mormont@gmail.com",
+    packages=["sldc_openslide"]
+)
